14df9fd806419340d4908df0ed063fc16481bc08
tungnguyen31403324 <tungnthe176064@fpt.edu.vn>
2025-06-18 13:18:05 +0700

    18thang6

--- src/main/java/com/example/exe2update/controller/ShopController.java
+++ src/main/java/com/example/exe2update/controller/ShopController.java
@@ -8,7 +8,6 @@ import com.example.exe2update.service.ProductService;
 import com.example.exe2update.service.ReviewService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
